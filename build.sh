5142ce8adcc5db1a0175845eff38e70cd23c672a
Sandu Bogdan <cornusandu@hotmail.com>
2025-11-02 21:44:23 +0200

    small build.sh change

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-g++ ./src/allocate/allocate.cpp -fPIC -shared -o ./dist/allocate.so -nostartfiles -Wl,--no-undefined -Wl,-z,defs
+g++ ./src/allocate/allocate.cpp -fPIC -shared -o ./dist/allocate.so -nostartfiles
 g++ ./src/kernel.cpp ./src/statemanager.cpp -fPIE -o ./dist/main.bin -Wl,-e,_boot
